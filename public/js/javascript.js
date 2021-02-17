f7361a558b0e5e455f1c7d87bd16ddac67a9eae9
justin372 <70275559+justin372@users.noreply.github.com>
2021-02-17 12:01:34 -0800

    changes are made

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,4 +1,53 @@
 function findBook(){
  var userSearch = document.getElementById('userInput').value;
- document.getElementById('result').innerHTML = userSearch;
+ var bookResult = document.getElementById('result');
+
+ bookResult.innerHTML = "";
+
+ $.ajax({
+ 	 type: "GET",
+ 	 url: "https://www.googleapis.com/books/v1/volumes?q=" + userSearch,
+ 	 dataType: 'JSON',
+ 	 success: function(book){
+       console.log(book);
+       for(var i = 0; book.items.length; i++){
+          var wrapperDiv = document.createElement('div');
+          wrapperDiv.className = 'media';
+          //create img ellement for images
+       	  var image = document.createElement('img');
+          image.className = '';
+       	  image.src = book.items[i].volumeInfo.imageLinks.thumbnail;
+          //create div element with call of media - body
+          var div  = document.createElement('div');
+          div.className = 'media-body';
+          //create header for body
+          var header = document.createElement('h4');
+          header.className = 'nextto1';
+          header.innerHTML = 'title : ' + book.items[i].volumeInfo.title;  
+          var author = document.createElement('h5');
+          author.className = 'nextto2';
+          author.innerHTML = 'Author : ' + book.items[i].volumeInfo.authors;       
+          var desc = document.createElement('p');
+          desc.className='';
+          desc.innerHTML = 'Description: ' + book.items[i].volumeInfo.description;
+          //append header to body
+          
+          wrapperDiv.appendChild(image);
+          bookResult.appendChild(header);
+          bookResult.appendChild(author);
+          wrapperDiv.appendChild(div);
+          
+          bookResult.appendChild(wrapperDiv);
+          bookResult.appendChild(desc);
+          //create line to seperate sections
+          var line = document.createElement('hr');
+          bookResult.appendChild(line);
+
+
+    
+       
+    
+    }
+ 	 }
+ })
 }
